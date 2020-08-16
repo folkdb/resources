e3c0ea89751a0ed54df57056a3ae00baaceced53
Daniel Narey <contact@danielnarey.com>
2020-08-15 23:12:02 -0400

    sync

--- test/index.js
+++ test/index.js
@@ -1,8 +1,31 @@
+
+import { resolve } from 'path';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+import {
+  listDirectories,
+  listFiles
+} from '../lib/filesystem-api/index.js';
+
+
+const fixtures = resolve(process.cwd(), 'test/fixtures');
 
 test('Always passes', () => {
   assert.ok(true);
+});
+
+test('listDirectories', () => {
+  assert.equal(
+    listDirectories(fixtures),
+    ['fruit', 'veggies'],
+  );
+});
+
+test('listFiles', () => {
+  assert.equal(
+    listFiles(fixtures),
+    ['cake.txt', 'cookies.txt'],
+  );
 });
 
 test.run();
