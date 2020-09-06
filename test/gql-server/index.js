3f8b23929de3cb2c8cd668a53a14102c263c51d5
Daniel Narey <contact@danielnarey.com>
2020-09-06 00:32:11 -0400

    sync

--- test/gql-server/index.js
+++ test/gql-server/index.js
@@ -1,17 +1,23 @@
+import { readFile } from 'fs/promises';
 import apolloServer from 'apollo-server';
+import { absolute } from '../lib/utils/path/index.js';
 
 const { ApolloServer } = apolloServer;
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
+export const mockServer = async () => {
+  const typeDefs = await readFile(absolute('schema/generated.gql'), 'utf8');
 
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-  },
+  const resolvers = {
+    Query: {
+      allResources: () => ({ 
+        data: [
+          { slug: 'apple' },
+          { slug: 'orange' },
+          { slug: 'pear' },
+        ],
+      }),
+    },
+  };
+
+  return new ApolloServer({ typeDefs, resolvers });
 };
-
-export const gqlServer = new ApolloServer({ typeDefs, resolvers });
--- test/graphql-api.js
+++ test/graphql-api.js
@@ -1,23 +1,31 @@
-import graphqlRequest from 'graphql-request';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+import graphqlRequest from 'graphql-request';
 import { gqlServer } from './gql-server/index.js';
 
 const { GraphQLClient } = graphqlRequest;
 
 test('Gets expected Hello response', async () => {
-  const httpServer = await gqlServer.listen();
-  const client = new GraphQLClient(httpServer.url);
+  const { url, server } = await gqlServer.listen();
+  const client = new GraphQLClient(url);
 
-  const response = await client.request(`
+  const { allResources } = await client.request(`
     {
-      hello(name: "Daniel")
+      allResources {
+        data {
+          slug
+        }
+      }
     }
   `);
   
-  assert.equal(response.hello, 'Hello Daniel');
+  assert.equal(allResources.data, [
+    { slug: 'apple' },
+    { slug: 'orange' },
+    { slug: 'pear' },
+  ]);
 
-  httpServer.server.close();
+  server.close();
 });
 
 export default test;
