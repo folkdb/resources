6eb7648d636350a0fae12e994e59ae70a78b1d60
Daniel Narey <contact@danielnarey.com>
2020-10-06 09:55:22 -0400

    sync

--- test/graphql-api.js
+++ test/graphql-api.js
@@ -10,15 +10,9 @@ const graphqlApiTests = suite('GraphQL API Tests');
 graphqlApiTests.before(async (context) => {
   const gqlServer = await mockServer();
   const { url, server } = await gqlServer.listen();
-  context.server = server;
   
   const { GraphQLClient } = graphqlRequest;
   context.client = new GraphQLClient(url);
-});
-
-graphqlApiTests.after((context) => {
-  const { server } = context;
-  server.close();
 });
 
 graphqlApiTests('allCategories', async (context) => {
