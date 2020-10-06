f68ad427cf82139e6a1313554808e9e5a35b199d
Daniel Narey <contact@danielnarey.com>
2020-10-06 12:06:06 -0400

    sync

--- test/filesystem-api.js
+++ test/filesystem-api.js
@@ -1,30 +1,31 @@
 import { resolve } from 'path';
-import { test } from 'uvu';
+import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import {
   listDirectories,
   listFiles
 } from '../lib/filesystem-api/index.js';
 
-
 const entry = resolve(process.cwd(), 'test/mock-files');
 
-test('Always passes', () => {
+const filesystemApiTests = suite('Filesystem API Tests');
+
+filesystemApiTests('Always passes', () => {
   assert.ok(true);
 });
 
-test('listDirectories', async () => {
+filesystemApiTests('listDirectories', async () => {
   assert.equal(
     await listDirectories(entry),
     ['fruit', 'veggies'],
   );
 });
 
-test('listFiles', async () => {
+filesystemApiTests('listFiles', async () => {
   assert.equal(
     await listFiles(entry),
     ['cake.txt', 'cookies.txt'],
   );
 });
 
-export default test;
+export default filesystemApiTests;
